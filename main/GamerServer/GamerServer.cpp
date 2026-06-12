707471759a39a0cf09e7144768bf10bd6468b543
mcchow <44908668+mcchow@users.noreply.github.com>
2026-06-12 14:03:42 -0700

    Update

---
+++ main/GamerServer/GamerServer.cpp
@@ -0,0 +1,12 @@
+﻿// GamerServer.cpp : Defines the entry point for the application.
+//
+
+#include "GamerServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
